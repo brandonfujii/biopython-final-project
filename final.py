dc5d50fb91d0dd024f2286ed1c8d919dd0afc50a
brandonfujii <brandonfujii2018@u.northwestern.edu>
2017-12-26 11:39:01 -0800

    completed, added orf test

--- final.py
+++ final.py
@@ -5,7 +5,6 @@ Final assignment for Python for Genomic Data Science @ University of Washington
 
 from sys import argv, exit
 from Bio import SeqIO
-from Bio.SeqRecord import SeqRecord
 
 class OpenReadingFrame(object):
     def __init__(self, record_number, record_id, sequence, start, frame = 1):
@@ -24,7 +23,6 @@ class OpenReadingFrame(object):
 class RecordCollection(object):
     def __init__(self, records):
         self.records = dict([(record.id, record) for record in records])
-        print self.records
         self.max_sequences = self.get_longest_sequence_records()
         self.min_sequences = self.get_shortest_sequence_records()
         self.orfs = []
@@ -74,12 +72,14 @@ class RecordCollection(object):
 
         self.orfs = orfs
         return sorted(orfs, key = lambda x: len(x), reverse=True)
-
-    def get_longest_orf(self):
-        return self.max_orf
     
-    def get_shortest_orf(self):
-        return self.min_orf
+    def find_all_repeats(self, repeat_length):
+        repeats = {}
+        for id, record in self.records.items():
+            if record.seq:
+                repeats = find_repeats(record.seq, repeat_length, repeats)
+        
+        return repeats
 
 def find_orf(record, frame = 1):
     start_indices = []
@@ -98,14 +98,15 @@ def find_orf(record, frame = 1):
 
     orfs = []
     last_index = 0
-    for i in range(0,len(start_indices)):
-        for j in range(0, len(stop_indices)):
-            if start_indices[i] < stop_indices[j] and start_indices[i] > last_index:
-                orf_sequence = sequence[start_indices[i]:stop_indices[j]+3]
-                orfs.append(OpenReadingFrame(record.record_number, record.id, orf_sequence, start_indices[i], frame))
-                last_index = stop_indices[j] + 3
+    for start_index in start_indices:
+        for stop_index in stop_indices:
+            if start_index < stop_index and start_index >= last_index:
+                orf_sequence = sequence[start_index:stop_index + 3]
+                orfs.append(OpenReadingFrame(record.record_number, record.id, orf_sequence, start_index, frame))
+                last_index = stop_index + 3
                 break
 
+    # print orfs
     return orfs
 
 def read_fasta_file(filename):
@@ -116,14 +117,72 @@ def read_fasta_file(filename):
         records.append(record)
     return records
 
+def find_repeats(sequence, repeat_length, repeats_dict = {}):
+    for i in range(len(sequence)):   
+        read = str(sequence[i:i + repeat_length])
+        if read and len(read) == repeat_length:
+            if read not in repeats_dict:
+                repeats_dict[read] = 1
+            else:
+                repeats_dict[read] += 1
+    return repeats_dict
+
+def find_max_in_dict(dictionary):
+    return max(zip(dictionary.values(), dictionary.keys())) if len(dictionary) else None
+
 def main():
     if len(argv) < 2:
         exit('Usage: %s filename.fasta' % argv[0])
 
     args = argv[1:]
     records = RecordCollection(read_fasta_file(filename = args[0]))
-    orfs = records.find_all_orfs(frame = 2)
-    print orfs[0], orfs[-1]
+
+    print "============ Question 1 ============"
+    """
+    Q1: How many records are in the multi-FASTA file?
+    """
+    print len(records)
+
+    print "============ Question 2 ============"
+    """
+    Q2: What is the length of the longest sequence in the file?
+    """
+    longest_sequences = records.get_longest_sequence_records()
+    print len(longest_sequences[0]) if longest_sequences else 0
+
+    print "============ Question 3 ============"
+    """
+    Q3: What is the length of the shortest sequence in the file?
+    """
+    shortest_sequences = records.get_shortest_sequence_records()
+    print len(shortest_sequences[0]) if shortest_sequences else 0
+
+    print "============ Question 4 ============"
+    """
+    Q4: What is the length of the longest ORF appearing in reading frame 2 of any of the sequences?
+    """
+    q4_orfs = records.find_all_orfs(frame = 2)
+    print len(q4_orfs[0])
+
+    print "============ Question 5 ============"
+    """
+    Q5: What is the starting position of the longest ORF in reading frame 3 in any of the sequences?
+    The position should indicate the character number where the ORF begins.
+    """
+    q5_orfs = records.find_all_orfs(frame = 3)
+    print q5_orfs[0].start if q5_orfs else None
+
+    print "============ Question 6 ============"
+    """
+    Q6: What is the length of the longest ORF appearing in any sequence and in any forward reading frame?
+    """
+    max_orf_length = 0
+    for frame in range(1, 4):
+        q6_orfs = records.find_all_orfs(frame = frame)
+        if len(q6_orfs[0]) > max_orf_length:
+            max_orf_length = len(q6_orfs[0])
+
+    print max_orf_length
 
     print "============ Question 7 ============"
     """
@@ -138,6 +197,35 @@ def main():
             max_orf_length = max_length
 
     print max_orf_length
-    
+
+    print "============ Question 8 ============"
+    """
+    Q8: Find the most frequently occurring repeat of length 6 in all sequences. How many times does it occur in all?
+    """
+    repeats = records.find_all_repeats(repeat_length = 6)
+    num_occurrences, most_occurring_repeat = find_max_in_dict(repeats)
+    print most_occurring_repeat, "occurs", num_occurrences, "times"
+
+    print "============ Question 9 ============"
+    """
+    Q9: Find all repeats of length 12 in the input file. Let's use Max to specify the number of copies
+    of the most frequent repeat of length 12. How many different 12-base sequences occur Max times?
+    """
+    repeats = records.find_all_repeats(repeat_length = 12)
+    num_occurrences, most_occurring_repeat = find_max_in_dict(repeats)
+    max_repeats = []
+    for key, value in repeats.items():
+        if value == num_occurrences:
+            max_repeats.append(key)
+    print len(max_repeats)
+
+    print "============ Question 10 ============"
+    """
+    Q10: Which one of the following repeats of length 7 has a maximum number of occurrences?
+    """
+    repeats = records.find_all_repeats(repeat_length = 7)
+    num_occurrences, most_occurring_repeat = find_max_in_dict(repeats)
+    print most_occurring_repeat
+
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,21 +1,29 @@
 import unittest
 from random import randrange, choice
-import uuid
+from uuid import uuid4
 from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
-from final import RecordCollection, read_fasta_file
+from final import RecordCollection, read_fasta_file, find_orf, find_repeats
 
-def generate_sequence(sequence_length):
+def generate_random_sequence(sequence_length):
     return ''.join(choice("ACGT") for _ in range(sequence_length))    
 
-def create_record(index, sequence_length = None, id = None):
-    sequence = generate_sequence(randrange(500, 5000) if not sequence_length else sequence_length)
-    record = SeqRecord(Seq(sequence), id = str(uuid.uuid4()) if not id else id)
+def create_random_record(index, sequence_length = None, id = None):
+    sequence = generate_random_sequence(randrange(500, 5000) if not sequence_length else sequence_length)
+    record = SeqRecord(Seq(sequence), id = str(uuid4()) if not id else id)
     record.record_number = index
     return record
 
-def generate_records(num_records):
-    return [create_record(i) for i in range(num_records)]
+def generate_random_records(num_records):
+    return [create_random_record(i) for i in range(num_records)]
+
+def generate_records(sequences):
+    records = []
+    for i, sequence in enumerate(sequences):
+        record = SeqRecord(Seq(sequence), id = str(uuid4()))
+        record.record_number = i
+        records.append(record)
+    return records
 
 class SequenceTestCase(unittest.TestCase):
     def test_fasta_read(self):
@@ -32,27 +40,26 @@ class SequenceTestCase(unittest.TestCase):
         num_records = 50
 
         for i in range(num_records):
-            id = str(uuid.uuid4())
-            records.append(create_record(i, sequence_length = None, id = id))
+            id = str(uuid4())
+            records.append(create_random_record(i, sequence_length = None, id = id))
             ids.append(id)
-            fake_ids.append(str(uuid.uuid4()))
+            fake_ids.append(str(uuid4()))
 
         records = RecordCollection(records)
-
         for i in range(num_records):
             self.assertIsInstance(records.get_record_by_id(ids[i]), SeqRecord)
             self.assertIsNone(records.get_record_by_id(fake_ids[i]))
 
     
     def test_longest_and_shortest_sequence_lengths(self):
-        records = generate_records(50)
+        records = generate_random_records(50)
 
         i = randrange(len(records) + 1)
-        max_length_record = create_record(i, sequence_length = 5001)
+        max_length_record = create_random_record(i, sequence_length = 5001)
         records.insert(i, max_length_record)
 
         i = randrange(len(records) + 1)
-        min_length_record = create_record(i, sequence_length = 499)
+        min_length_record = create_random_record(i, sequence_length = 499)
         records.insert(i, min_length_record)
 
         records = RecordCollection(records)
@@ -64,8 +71,41 @@ class SequenceTestCase(unittest.TestCase):
         self.assertEqual(min_length_record.id, shortest_records[0].id)    
 
     def test_orf(self):
-        records = read_fasta_file("dna2.fasta")
-        records = RecordCollection(records)
+        sequences_with_orf = [
+            "ATGCCGAAATGA",
+            "ATGCNATAAAATGCCCGCGTGATAAC",
+            "AAATGCGTTAA"
+        ]
+
+        expected_results = {}
+        expected_results["1"] = [
+            "ATGCCGAAATGA",
+            "ATGCNATAA",
+            ""
+        ]
+        expected_results["2"] = [
+            "",
+            "ATGCCCGCGTGA",
+            ""
+        ]
+        expected_results["3"] = [
+            "",
+            "",
+            "ATGCGTTAA"
+        ]
+
+        records_with_orf = generate_records(sequences_with_orf)
+        for i, record in enumerate(records_with_orf):
+            for frame in range(1, 4):
+                orfs = find_orf(record, frame = frame)
+                expected = expected_results[str(frame)][i]
+                if not orfs and not expected:
+                    continue
+                
+                if not orfs:
+                    self.fail("Expected %s but received empty ORFs for record at index %i" % (expected, i))
+
+                self.assertEqual(str(orfs[0].sequence), expected)
 
 if __name__ == '__main__':
     unittest.main()
